8b665146dfb9ea03f140e6c0a03d86cfc64d9a38
GiorgiaS <giorgia-sir95@outlook.com>
2024-07-21 12:48:59 +0200

    Update page 2

--- pages/page2.py
+++ pages/page2.py
@@ -6,12 +6,12 @@ st.markdown("## Scenario 1:")
 st.markdown('Imagine you need to access SoftOrg\'s metaverse workspace to meet with representatives from a key client factory. Your manager and three colleagues will also be attending the meeting. In total, there will be five *SoftOrg* employees, including yourself, and two customer representatives connected to the virtual space.')
 st.markdown('''You and other participants must share, among others, data related to your physical aspect (i.e., the body shape) to access the virtual working space.  
         The policy is the following:  
-        - Purpose = {Prp1, ….}
-        <sup style="font-size: smaller;" I,e., for which purpose data are collected by the metaverse working space. <\sup>  
-        - Retention = 90 day
-        <sup style="font-size: smaller;" I.e., time after which data are removed <\sup>  
-        - Third party = {tp1, ...}
-        <sup style="font-size: smaller;" I.e., to which parties external to the metaverse working space data are shared <\sup>
+        - Purpose = {Prp1, ...}  
+        <sup><sub>I,e., for which purpose data are collected by the metaverse working space.</sub></sup>   
+        - Retention = 90 day  
+        <sup><sub>I.e., time after which data are removed</sub></sup>  
+        - Third party = {tp1, ...}  
+        <sup><sub>I.e., to which parties external to the metaverse working space data are shared</sub></sup>  
         ''')
 
 
