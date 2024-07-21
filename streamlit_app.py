f0e4d2c59bfccd8691b4e99e67a9d1f69c1976db
GiorgiaS <giorgia-sir95@outlook.com>
2024-07-21 11:26:20 +0200

    Update first page

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,16 @@ imagePath = "./images/MetaversePlaza.jpeg"
 st.image(imagePath)
 
 # Set text:
-text1 = 'The Metaverse is a virtual environment where people join to do different kinds of activities, like meeting with friends, playing virtual games, and participating in social events.' 
-text2 = 'Generally, people access using Virtual Reality headset, from home or anywhere they are.'
-st.markdown('# Title')
-st.markdown('Survey description/purpose...')
+st.markdown('# Privacy in the Metaverse')
 st.markdown("## Metaverse")
-st.markdown(text1)
-st.markdown(text2)
-st.markdown('## Metaverse Working Space')
-st.markdown('Explain the metaverse working space')
+st.markdown('The Metaverse is a virtual environment where people join to do different kinds of activities, like meeting with friends, playing virtual games, and participating in social events. Generally, people can access a virtual reality headset from home or anywhere.')
 st.markdown('## Survey')
-st.markdown('We will assume that you are an employee of a software organisation called SoftOr, and you need to access the working room. Then, we will show you two different contexts and ask you to select your privacy preferences related to the data required by the working room. For the purpose of the survey, we consider the policy of a single data, namely, your body shape – i.e., since you are accessing a working environment, you will be represented by an avatar that is close to your real physical aspect.')
+st.markdown('Let us assume you are a metaverse user and need access to a space with other people where you must share your personal data with them. This data consists of the requirements of the specific virtual space, which are specified through policies.')
+st.markdown('Would you share all the required data? To which extend would you agree with the policies?')
+st.markdown('We aim to create a system that respects everyone\'s privacy. This system will manage and balance the privacy preferences of all participants, including yours, along with the requirements of the shared space. In this survey, we will present two scenarios in the metaverse where you will set your privacy preferences. We will then integrate your choices with those of other users and the space\'s policies. Finally, we will ask you a few questions about how you feel about the new policy.')
+st.markdown('## Scenario: Metaverse Working Space')
+st.markdown('A metaverse working space (like Meetaverse) is a virtual 3D room where people can join to collaborate, organise meetings, and work together. The working room has some requirements to improve participants\' experiences, which are stated in the policies.')
+st.markdown('We will assume that you are an employee of a software organisation called *SoftOr*, and you must access the working room. Then, we will show you two different contexts and ask you to select your privacy preferences related to the data the working room requires. For the survey, we consider the policy of a single data, namely, your body shape – i.e., since you are accessing a working environment, you will be represented by an avatar that is close to your real physical aspect.')
 
 
 # st.page_link("./pages/page1.py", label="Start")
