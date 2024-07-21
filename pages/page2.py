c9b0f16be6fc196f875c5354aaafeae2f040df24
GiorgiaS <giorgia-sir95@outlook.com>
2024-07-21 12:54:47 +0200

    Update page 2

--- pages/page2.py
+++ pages/page2.py
@@ -7,11 +7,11 @@ st.markdown('Imagine you need to access SoftOrg\'s metaverse workspace to meet w
 st.markdown('''You and other participants must share, among others, data related to your physical aspect (i.e., the body shape) to access the virtual working space.  
         The policy is the following:  
         - Purpose = {Prp1, ...}  
-        <sup><sub>I,e., for which purpose data are collected by the metaverse working space.</sub></sup>   
+        ##### I,e., for which purpose data are collected by the metaverse working space.  
         - Retention = 90 day  
-        <sup><sub>I.e., time after which data are removed</sub></sup>  
+        ##### I.e., time after which data are removed.  
         - Third party = {tp1, ...}  
-        <sup><sub>I.e., to which parties external to the metaverse working space data are shared</sub></sup>  
+        ##### I.e., to which parties external to the metaverse working space data are shared.  
         ''')
 
 
